efd57997b227624277de0856ef29d60bd2b60a91
Tushar Patil <tusharwankhede02@gmail.com>
2025-06-25 20:20:56 +0530

    Delete Percentage caculator.py

--- Percentage caculator.py
+++
@@ -1,9 +0,0 @@
-physics=int(input("enter marks for physics:"))
-chemistry=int(input("enter marks for chemistry:"))
-maths=int(input("enter marks for maths:"))
-biology=int(input("enter marks for biology:"))
-cs=int(input("enter marks for cs:"))
-totalmarks=(physics+chemistry+maths+biology+cs)
-percentage= (totalmarks/500)*100
-print("you percentage:",percentage)
-
