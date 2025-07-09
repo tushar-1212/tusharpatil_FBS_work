690d5586de24f76479209615ee6ae71b5f9c755a
Tushar Patil <tusharwankhede02@gmail.com>
2025-07-09 18:56:03 +0530

    Delete Workspace/Assignment 2 directory

--- Workspace/Assignment 2/Assig-2 temperature conversion.py
+++
@@ -1,4 +0,0 @@
-#Convert temp from Celsius to Fahrenheit.
-celcius = float(input("Enter the temperature in Celsius: "))
-fahrenheit = (celcius * 9/5) + 32
-print("Temperature in Fahrenheit:", fahrenheit)
--- Workspace/Assignment 2/Assig-5 selling price after discount.py
+++
@@ -1,6 +0,0 @@
-#calculate selling price of book based on cost price and discount
-
-cp= float(input("Enter cost price of book: "))
-discount= float(input("Enter discount percentage: "))
-sp= cp - (cp * discount / 100)
-print("Selling price of book after discount is:", sp)
