9f3fec331e9dac9cdfce23e4bc1b3476283a1dde
Tushar Patil <tusharwankhede02@gmail.com>
2025-07-09 18:56:20 +0530

    Delete Workspace/Assignment 3 directory

--- Workspace/Assignment 3/Assig-1 check num for positive or neg.py
+++
@@ -1,9 +0,0 @@
-#check the given number for negative of positive 
-
-num=float(input("Enter the num:"))
-if num==0:
-    print("The number is unique")
-elif num<0 :
-    print("The number is negative")
-else:
-    print("The number is positive")
--- Workspace/Assignment 3/assig-2 vowel or consonant.py
+++
@@ -1,8 +0,0 @@
-#a program to input any alphabet and check whether it is vowel or consonant.
-
-alpha=input("Enter any alphabet:")
-if alpha in 'aeiouAEIOU':
-    print("The alphabet is a Vowel")
-
-else:
-    print("The alphabet is a Consonant")
